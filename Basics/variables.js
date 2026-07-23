3e77adfb29797fc45f5e7850d9b3faf1c081050e
vik <theheero007@Gmail.com>
2026-07-23 20:25:17 +0000

    variables

---
+++ Basics/variables.js
@@ -0,0 +1,12 @@
+const accountId = 12      //  const fixez the value, & we can't change the value of the variable.
+let accEmail = "abc@gmail.com"
+var accPass = "123"
+accCity = "jaipur"
+
+//accountId = 2
+
+console.log(accountId);
+
+// prefer not to use var , due to block scope problem.
+ 
+console.table([accountId, accEmail, accPass])
