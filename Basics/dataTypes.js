cc1d8c8efe169627733d166ca44439bbe004bd11
vik <theheero007@Gmail.com>
2026-07-24 19:47:12 +0000

    dataTypes

---
+++ Basics/dataTypes.js
@@ -0,0 +1,8 @@
+// Two - types:  1. Primitive 2. non - primitive
+
+// Primitive
+// & types : String , Number, Boolean, null, undefined, Symbol, BigInt
+
+
+// Non-primitive ( reference - type )
+// Array, Object, Function
