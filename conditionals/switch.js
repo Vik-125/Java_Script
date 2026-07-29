625653779b27f24a6fbd3a1cf96335ca5e04cc14
vik <theheero007@Gmail.com>
2026-07-29 07:20:52 +0000

    switch Case

---
+++ conditionals/switch.js
@@ -0,0 +1,15 @@
+let key = 3
+
+switch(key){
+    case 0:
+        console.log("Value is 0")
+        break;
+    case 1:
+        console.log("value is 1")
+        break;
+    case 2 : 
+        console.log("value is 2")
+        break;
+    default:
+        console.log("None")
+}
